ea8324e508f2e73e9c20143459d6f618f05489d6
thePmSquare <impmmmm@gmail.com>
2020-09-30 21:22:28 +0530

    add material ui

--- src/Game.js
+++ src/Game.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import { Swipeable } from "react-swipeable";
 import "./stylesheets/Game.css";
 class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dir: "",
+    };
+  }
   handleSwipe = (eventData) => {
-    console.log(eventData.dir);
+    this.setState(() => {
+      return {
+        dir: eventData.dir,
+      };
+    });
   };
   render = () => {
     return (
       <div className="Game">
         <Swipeable onSwiped={this.handleSwipe}>
           <h1>Game</h1>
+          {this.state.dir}
         </Swipeable>
       </div>
     );
