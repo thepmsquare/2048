438a66189421f4b8c4e80f6f4a806caf1f5740b5
thePmSquare <impmmmm@gmail.com>
2020-10-04 00:58:17 +0530

    add snackbar instructions for desktop

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { Swipeable } from "react-swipeable";
+import { isBrowser } from "react-device-detect";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import "./stylesheets/Game.css";
 import TitleRow from "./TitleRow";
 import Selector from "./Selector";
@@ -10,15 +14,24 @@ class Game extends Component {
     super(props);
     this.state = {
       gridSize: 4,
+      snackbar: true,
     };
   }
   handleSwipe = (eventData) => {
     console.log(eventData.dir);
   };
+  handleKeyPress = (eventData) => {
+    console.log(eventData.key);
+  };
+  handleSnackbarClose = () => {
+    this.setState(() => {
+      return { snackbar: false };
+    });
+  };
   render = () => {
     return (
       <Swipeable onSwiped={this.handleSwipe}>
-        <div className="Game">
+        <div className="Game" tabIndex={0} onKeyDown={this.handleKeyPress}>
           <TitleRow />
           <div className="Game-secondRow">
             <Selector />
@@ -27,6 +40,30 @@ class Game extends Component {
 
           <Board size={this.state.gridSize} />
         </div>
+        {isBrowser && (
+          <Snackbar
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            open={this.state.snackbar}
+            autoHideDuration={6000}
+            onClose={this.handleSnackbarClose}
+            message="Use AWSD or Arrow Keys to Play."
+            action={
+              <React.Fragment>
+                <IconButton
+                  size="small"
+                  aria-label="close"
+                  color="inherit"
+                  onClick={this.handleSnackbarClose}
+                >
+                  <CloseIcon fontSize="small" />
+                </IconButton>
+              </React.Fragment>
+            }
+          />
+        )}
       </Swipeable>
     );
   };
