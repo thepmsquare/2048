2449261978b80ab72a4dad2f35947741ec4c8613
thePmSquare <impmmmm@gmail.com>
2020-10-06 16:10:08 +0530

    add colors after 2048 in a repeating pattern

--- src/Board.js
+++ src/Board.js
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import "./stylesheets/Board.css";
-import colorPalette from './constant';
+import colorPalette from "./constant";
 class Board extends Component {
-
-  getColor(value){
-     for (let palette in colorPalette){
-       if(parseInt(palette)===value){
-         return colorPalette[palette]
-       }
-       if(parseInt(palette)>2048){
-        return colorPalette[2048]
-       }
-     }
+  getColor(value) {
+    let compareThis = Math.log2(value);
+    while (compareThis > 11) {
+      compareThis -= 11;
+    }
+    for (let palette in colorPalette) {
+      if (parseInt(palette) === compareThis) {
+        return colorPalette[palette];
+      }
+    }
   }
 
   render = () => {
@@ -26,10 +26,17 @@ class Board extends Component {
           gridTemplateRows: `repeat(${size}, 1fr)`,
         }}
       >
-        {board.map((block) => {         
+        {board.map((block) => {
           return (
-            <Paper style={{backgroundColor:this.getColor(block.value),color:'#FFF'}} key={`${block.row}-${block.col}`} className="Board-block">
-              <Typography > {block.value} </Typography>
+            <Paper
+              style={{
+                backgroundColor: this.getColor(block.value),
+                color: "#FFF",
+              }}
+              key={`${block.row}-${block.col}`}
+              className="Board-block"
+            >
+              <Typography> {block.value} </Typography>
             </Paper>
           );
         })}
--- src/constant.js
+++ src/constant.js
@@ -1,14 +1,14 @@
-const colorPalette={
-    2:'#66bb6a',
-    4:'#26a69a',
-    8:'#26c6da',
-    16:'#29b6f6',
-    32:'#42a5f5',
-    64:'#5c6bc0',
-    128:'#7e57c2',
-    256:'#ffca28',
-    512:'#ab47bc',
-    1024:'#ec407a',
-    2048:'#ef5350'
-}
-export default colorPalette
+const colorPalette = {
+  1: "#66bb6a",
+  2: "#26a69a",
+  3: "#26c6da",
+  4: "#29b6f6",
+  5: "#42a5f5",
+  6: "#5c6bc0",
+  7: "#7e57c2",
+  8: "#ffca28",
+  9: "#ab47bc",
+  10: "#ec407a",
+  11: "#ef5350",
+};
+export default colorPalette;
