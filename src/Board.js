a0c1f0b0cf7fe1a4f001e8c3f71db4bdb920a0ed
Sankalpa Fernando <sankalpafernando2017@gmail.com>
2020-10-06 13:10:32 +0530

    w/signoff
    Signed-off-by: Sankalpa Fernando <sankalpafernando2017@gmail.com>

--- src/Board.js
+++ src/Board.js
@@ -2,8 +2,17 @@ import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import "./stylesheets/Board.css";
-
+import colorPalette from './constant';
 class Board extends Component {
+
+  getColor(value){
+     for (let palette in colorPalette){
+       if(parseInt(palette)===value){
+         return colorPalette[palette]
+       }
+     }
+  }
+
   render = () => {
     const { size, board } = this.props;
     return (
@@ -14,10 +23,10 @@ class Board extends Component {
           gridTemplateRows: `repeat(${size}, 1fr)`,
         }}
       >
-        {board.map((block) => {
+        {board.map((block) => {         
           return (
-            <Paper key={`${block.row}-${block.col}`} className="Board-block">
-              <Typography> {block.value} </Typography>
+            <Paper style={{backgroundColor:this.getColor(block.value),color:'#FFF'}} key={`${block.row}-${block.col}`} className="Board-block">
+              <Typography component="h4" variant="h4" > {block.value} </Typography>
             </Paper>
           );
         })}
