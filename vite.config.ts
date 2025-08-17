bdf06d7fd42151c35d71258646c04578bcd2a72d
Parth Mangtani <impmmmm@gmail.com>
2025-08-17 17:03:11 +0530

    ✨ pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "2048",
+        short_name: "2048",
+        description: "2048 Game",
+        theme_color: "#0FF",
+        background_color: "#000",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "2048.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+          {
+            src: "2048.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+          {
+            src: "2048.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
